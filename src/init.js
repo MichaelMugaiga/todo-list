b3d0e9eb5e6410abd7042328e876c0aa4488bb30
Mugaiga <mmugaiga@gmail.com>
2023-01-18 14:50:17 +0100

    progress on the todo  list assignement

--- src/init.js
+++ src/init.js
@@ -1,1 +1,35 @@
 
+    const taskForm = document.getElementById("task-form");
+    const taskInput = document.getElementById("task-input");
+    const taskList = document.getElementById("task-list");
+    let tasks = [];
+
+    taskForm.addEventListener("submit", addTask);
+
+    function addTask(e) {
+      e.preventDefault();
+      const task = taskInput.value;
+      tasks.push(task);
+      renderTasks();
+      taskInput.value = "";
+    }
+
+    function renderTasks() {
+      taskList.innerHTML = "";
+      tasks.forEach((task, index) => {
+        const li = document.createElement("li");
+        li.innerHTML = `${task} <button onclick="editTask(${index})">Edit</button> <button onclick="deleteTask(${index})">Delete</button>`;
+        taskList.appendChild(li);
+      });
+    }
+
+    function editTask(index) {
+      const newTask = prompt("Enter new task:");
+      tasks[index] = newTask;
+      renderTasks();
+    }
+
+    function deleteTask(index) {
+      tasks.splice(index, 1);
+      renderTasks();
+    }
